35f39001efd53b32ec4fea0b8fa2c7622fb3920f
venkatasai777 <venkatasaim16@gmail.com>
2024-06-02 13:46:56 +0530

    made changes for PORT and url with homepage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path')
 
 const app = express() ;
 
-const PORT = 4000 ;
+const PORT = process.env.PORT || 4000 ;
 
 dotEnv.config()
 
@@ -29,6 +29,6 @@ app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`)
 });
 
-app.use("/home", (req, res) => {
+app.use("/", (req, res) => {
     res.send("Welcome to Node")
 })
