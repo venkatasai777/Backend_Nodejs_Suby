99458cd69c38d79add18006a17973e80805fc51d
Venkata sai M <venkatasaim16@gmail.com>
2024-06-07 13:50:11 +0530

    Update firmController.js

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,65 +1,79 @@
 
-
-const Firm = require("../models/Firm");
-const Vendor = require("../models/Vendor");
-const multer = require("multer");
+const Firm = require('../models/Firm');
+const Vendor = require('../models/Vendor');
+const multer = require('multer');
 const path = require('path');
 
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, '/uploads/');
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/'); // Destination folder where the uploaded images will be stored
     },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Generating a unique filename
     }
 });
 
-// Initialize Multer with the storage configuration
 const upload = multer({ storage: storage });
 
-const addFirm = async (req, res) => {
+const addFirm = async(req, res) => {
     try {
-        const {firmName, area, category, region, offer} = req.body
-        console.log(req)
-        const image =  req.file.name
+        const { firmName, area, category, region, offer } = req.body;
+
+        const image = req.file ? req.file.filename : undefined;
+
         const vendor = await Vendor.findById(req.vendorId);
         if (!vendor) {
-            res.status(404).json({message: "Vendor Not Found"})
-        }else {
-                const firm = new Firm({
-                    firmName, area, category, region , offer, image, vendor: vendor._id
-                })
-                const savedFirm = await firm.save();
-                vendor.firm.push(savedFirm)
-        
-                await vendor.save();
-        
-                return res.status(200).json({message: "Firm added successfully", obj: savedFirm});
-                // return res.status(405).json({message: "Firm Already exists"});
+            res.status(404).json({ message: "Vendor not found" })
         }
-    }catch(error) {
-        console.error(error) ;
-        if (error.code === 11000) {
-            return res.status(500).json({message : "Firm Already Exists"});
+
+        if (vendor.firm.length > 0) {
+            return res.status(400).json({ message: "vendor can have only one firm" });
         }
-        return res.status(500).json({message : "Internal server Error"});
+
+        const firm = new Firm({
+            firmName,
+            area,
+            category,
+            region,
+            offer,
+            image,
+            vendor: vendor._id
+        })
+
+        const savedFirm = await firm.save();
+
+        const firmId = savedFirm._id
+        const vendorFirmName = savedFirm.firmName
+
+        vendor.firm.push(savedFirm)
+
+        await vendor.save()
+
+
+
+        return res.status(200).json({ message: 'Firm Added successfully ', firmId, vendorFirmName });
+
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json("intenal server error")
     }
-    
 }
 
-
-const deleteFirmById = async (req, res) => {
+const deleteFirmById = async(req, res) => {
     try {
         const firmId = req.params.firmId;
-        const deleteFirm = await Firm.findByIdAndDelete(firmId) ;
-        if (!deleteFirm) {
-            return res.status(404).json({error : "No Firm Found"});
+
+        const deletedProduct = await Firm.findByIdAndDelete(firmId);
+
+        if (!deletedProduct) {
+            return res.status(404).json({ error: "No product found" })
         }
-        
-    }catch(error) {
-            console.error(error)
-            res.status(500).json({error : "Internal server Error"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" })
     }
 }
 
-module.exports = {addFirm: [upload.single('image'), addFirm], deleteFirmById}
+module.exports = { addFirm: [upload.single('image'), addFirm], deleteFirmById }
